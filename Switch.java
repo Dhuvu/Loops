437613bad1a7680c559d459852da58690407111d
Dhruvisha Patel <Dhruvishapatel3008@gmail.com>
2022-08-02 23:13:38 +0100

    Loops Programs

--- Switch.java
+++ Switch.java
@@ -1,5 +1,31 @@
 package Loops;
 
 public class Switch {
+    public static void main(String[] args) {
+        int month=5;
+        switch (month){
+            case 1:
+                System.out.println("january");
+                break;
+            case 2:
+                System.out.println("feb");
+                break;
+            case 3:
+                System.out.println("march");
+                break;
+            case 4:
+                System.out.println("april");
+                break;
+                case 5:
+                System.out.println("may");
+                break;
+            case 6:
+                System.out.println("june");
+                break;
+            case 7:
+                System.out.println("july");
+                break;
 
+        }
+    }
 }
